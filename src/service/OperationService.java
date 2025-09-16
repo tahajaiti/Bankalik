eb14d7345b06772435d3ac4893798ffc67fcd09a
taha jaiti <kyojin.rar@gmail.com>
2025-09-16 17:06:53 +0100

    FEAT: implemnted the operationservice with deposit transfer and withdrawal

--- src/service/OperationService.java
+++ src/service/OperationService.java
@@ -1,9 +1,42 @@
 package service;
 
-public class OperationService {
+import contract.IAccountService;
+import contract.IOperationService;
+import model.account.Account;
+import model.operation.*;
 
-	public OperationService() {
-		// TODO Auto-generated constructor stub
+public class OperationService implements IOperationService {
+
+	private IAccountService accountService;
+	
+	public OperationService(IAccountService accountService) {
+		this.accountService = accountService;
+	}
+	
+	public void desposit(Account acc, double amount, String source) {
+		Operation operation = new Deposit(amount,source);
+		acc.deposit(operation);
+	}
+	
+	public void withdraw(Account acc, double amount, String dest) {
+		Operation operation = new Withdrawal(amount, dest);
+
+		acc.withdraw(operation);
+	}
+	
+	public void transfer(Account acc,double amount,int targetId, String reason) {
+		Operation opOne = new Withdrawal(amount,reason);
+		Operation opTwo = new Deposit(amount, reason);
+		
+		Account targetAccount = accountService.getAccount(targetId);
+		
+		if (targetAccount == null) {
+			throw new IllegalArgumentException("Error: Invalid target account");
+		}
+		
+		acc.withdraw(opOne);
+		
+		targetAccount.deposit(opTwo);
 	}
 
 }
