edc15a9dcece210d47ef2c4ded97312c74c62c75
taha jaiti <kyojin.rar@gmail.com>
2025-09-16 12:09:14 +0100

    FEAT: implement the operation base abstract class with base setters and getters

---
+++ src/model/operation/Operation.java
@@ -0,0 +1,43 @@
+package model.operation;
+
+import java.util.UUID;
+import java.time.LocalDateTime;
+
+public abstract class Operation {
+
+	protected UUID id;
+	protected LocalDateTime date;
+	protected double amount;
+	
+	protected Operation(double amount) {
+        this.setId(UUID.randomUUID());
+        this.setDate(LocalDateTime.now());
+        this.setAmount(amount);
+    }
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	
+}
